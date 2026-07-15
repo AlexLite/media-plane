5401b75b71e9b5230fc3be282e51d95820636c21
Alex Alexandrov <pikuloki@gmail.com>
2026-07-15 21:51:34 +0000

    fix(web): preserve FreeFrame iframe origin for handshake

--- apps/web/core/components/issues/issue-detail/freeframe-review-embed.tsx
+++ apps/web/core/components/issues/issue-detail/freeframe-review-embed.tsx
@@ -237,7 +237,7 @@ export function FreeFrameReviewEmbed(props: Props) {
         onLoad={postSessionToIframe}
         className="block w-full border-0"
         style={{ height }}
-        sandbox="allow-scripts allow-forms allow-popups"
+        sandbox="allow-scripts allow-same-origin allow-forms allow-popups"
         allow="fullscreen"
       />
     </section>
